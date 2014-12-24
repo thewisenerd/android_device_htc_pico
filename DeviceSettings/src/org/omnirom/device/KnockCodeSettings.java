3a4b30146257b75964a38ae0613e1615c83d71c7
Vineeth Raj <contact.twn@openmailbox.org>
2014-12-25 00:40:57 +0530

    tmp init, little steps in a right direction hopefully

---
+++ DeviceSettings/src/org/omnirom/device/KnockCodeSettings.java
@@ -0,0 +1,17 @@
+package org.omnirom.device;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class KnockCodeSettings extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.xml.main2);
+    }
+
+}
